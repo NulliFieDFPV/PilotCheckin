5c23fad700f9859d92bc7e5def010a0ff0db6ec7
NulliFieDFPV <demanufactures@web.de>
2018-05-01 20:37:34 +0200

    .

--- checkinstarter.py
+++ checkinstarter.py
@@ -1,2 +1,27 @@
 import time
 
+
+class Pilot(object):
+
+    def __init__(self):
+        pass
+
+class cCheckIn(object):
+
+
+    def __init__(self):
+        
+        self.__active = True
+
+        self.__listen()
+
+
+    def __listen(self):
+
+        while self.__active:
+
+            time.sleep(0.001)
+
+if __name__=="__main__":
+
+    Checkin=cCheckIn()
