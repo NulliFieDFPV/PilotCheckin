f708e2a54fcc3f0f299d1f28299c07762ae74123
NulliFieDFPV <demanufactures@web.de>
2018-05-10 14:04:57 +0200

    db config eingefügt

---
+++ CentralUnit/config/cfg_db.py
@@ -0,0 +1,16 @@
+
+
+host="localhost"  # your host, usually localhost
+user="udbpilot" # your username
+passwd="pdbpilot" # your password
+db="dbpilotcheckin" # name of the data base
+
+
+tables={}
+tables["pilots"]="tpilots"
+tables["races"]="traces"
+tables["waitlist"]="twaitlist"
+tables["attendance"]="tattendance"
+tables["channels"]="tchannels"
+tables["raceoptions"]="traceoptions"
+tables["rfid"]="trfid"
