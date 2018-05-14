5f80d77ab7cacdd1068e2bd57f63085543e97439
NulliFieDFPV <demanufactures@web.de>
2018-05-14 22:37:21 +0200

    WS2812 implementiert

--- ReaderUnit/test/led/led.ino
+++ ReaderUnit/test/led/led.ino
@@ -2,12 +2,13 @@
 
 #include <avr/power.h>
  
-#define PIN            6
+#define PIN            2
 #define NUMPIXELS      4
  
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
  
-int delayval = 500;
+int delayval = 100;
+int delayval2 = 1;
  
 void setup() {
   pixels.begin();
@@ -18,5 +19,41 @@ void loop() {
     pixels.setPixelColor(i, pixels.Color(255,255,255)); // Moderately bright green color.
     pixels.show(); // This sends the updated pixel color to the hardware.
     delay(delayval); // Delay for a period of time (in milliseconds).
+  }
+
+  for(int i=0;i<NUMPIXELS;i++){
+    pixels.setPixelColor(i, pixels.Color(255,0,0)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval); // Delay for a period of time (in milliseconds).
+  }
+
+  for(int i=0;i<NUMPIXELS;i++){
+    pixels.setPixelColor(i, pixels.Color(0,255,0)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval); // Delay for a period of time (in milliseconds).
+  }
+
+  for(int i=0;i<NUMPIXELS;i++){
+    pixels.setPixelColor(i, pixels.Color(0,0,255)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval); // Delay for a period of time (in milliseconds).
+  }
+
+  for(int i=0;i<NUMPIXELS;i++){
+    pixels.setPixelColor(i, pixels.Color(0,0,0)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval); // Delay for a period of time (in milliseconds).
+  }
+  
+  for(int i=0;i<NUMPIXELS;i++){
+    for (int i2=0;i2<=255;i2++) {
+      for (int i3=0;i3<=255;i3++) {
+        for (int i4=0;i4<=255;i4++) {
+          pixels.setPixelColor(i, pixels.Color(i2,i3,i4)); // Moderately bright green color.
+          pixels.show(); // This sends the updated pixel color to the hardware.
+          delay(delayval2); // Delay for a period of time (in milliseconds).
+        }
+      }
+    }
   }
 }
