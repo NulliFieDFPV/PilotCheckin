b63c97b3834f7229fa4941409be68cf97f999481
NulliFieDFPV <demanufactures@web.de>
2018-05-18 16:43:40 +0200

    neuer Branch für i2c

--- CentralUnit/classes/classNode.py
+++ CentralUnit/classes/classNode.py
@@ -30,10 +30,19 @@ class SlaveNode(cChannel):
         self.__msg_temp=""
 
         if self.port !="":
-            try:
-                self.__con= serial.Serial(self.port, 9600, timeout=5)
-            except:
-                pass
+            if self.__typ=="usb":
+                try:
+                    self.__con= serial.Serial(self.port, 9600, timeout=5)
+                except:
+                    pass
+
+            elif self.__type=="i2c":
+                try:
+                    pass
+                    # self.__con=i2c connector
+                except:
+                    pass
+
 
         self.__thListen=threading.Thread(target=self.__listenToNode,args=())
         self.__thListen.start()
