4f430b36816a8a8b4e77332520b676e2b5a33b8f
NulliFieDFPV <demanufactures@web.de>
2018-05-19 14:31:28 +0200

    angelegt

---
+++ webserver/www/index.php
@@ -0,0 +1,22 @@
+ 
+<?
+
+function generateRaceCbo($raceid) {
+
+    $cbo="&nbsp;asdf";
+
+    return $cbo;
+}
+
+$cbo=generateRaceCbo();
+
+?>
+
+<html>
+<head></head>
+<body>
+
+    <section><? echo $cbo; ?></<section>
+
+</body>
+</html>
