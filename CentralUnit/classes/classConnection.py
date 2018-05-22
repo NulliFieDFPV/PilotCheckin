3f830a1e4107e0695f2cff25f7d787b46e2da449
NulliFieDFPV <demanufactures@web.de>
2018-05-22 19:52:04 +0200

    i2c tests

--- CentralUnit/classes/classConnection.py
+++ CentralUnit/classes/classConnection.py
@@ -127,6 +127,7 @@ class cConI2C(object):
         try:
             self.__bus.write_block_data(self.__address, cmd, vals) # 5 = I/O error
         except IOError, err:
+                print err
                 return -1
         return 0
 
