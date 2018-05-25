bf81f9e7fe8a8fb97f56186b66d03f48f569a51d
NulliFieDFPV <demanufactures@web.de>
2018-05-25 18:00:10 +0200

    Warteliste

--- webserver/www/index.php
+++ webserver/www/index.php
@@ -1,22 +1,71 @@
  
 <?
+error_reporting(E_ALL);
+include "includes/monitor.inc.php";
+
+$raceid=0;
+if (isset($_GET["race"])) {
+    $raceid=$_GET["race"];
+}
+
+
+if ($raceid>0) {
+    header("Refresh: 3; URL=index.php?race=".$raceid);
+}
+
 
 function generateRaceCbo($raceid) {
 
-    $cbo="&nbsp;asdf";
+    $db = new PDO('mysql:host=localhost;dbname=dbpilotcheckin', 'udbpilot', 'pdbpilot');
+
+    $sql = "SELECT * FROM traces WHERE status=-1 ORDER BY race_name";
+
+
+    $cbo="<select id=\"race\" name=\"race\">";
+
+    foreach ($db->query($sql) as $row) {
+
+        $cbo .="<option ";
+        if ($raceid==$row["RID"]) {
+            $cbo .="selected ";
+        }
+        $cbo .="value=\"".$row["RID"]."\">".$row['race_name']."</option>";
+    }
+
+    $cbo .="</select>";
 
     return $cbo;
 }
 
-$cbo=generateRaceCbo();
+$cbo=generateRaceCbo($raceid);
+
+
 
 ?>
 
 <html>
-<head></head>
+<head>
+
+</head>
+
 <body>
 
-    <section><? echo $cbo; ?></<section>
+    <section>
+    <form action="index.php" method="get">
+      <? echo $cbo; ?>
+      <input type="submit" value="Laden">
+    </form>
+    </<section>
+
+    <?
+    if ($raceid>0) {
+        //Teilnehmer laden
+
+        //Monitor laden
+        echo ladeTabelle($raceid);
+
+    }
+    ?>
 
 </body>
 </html>
