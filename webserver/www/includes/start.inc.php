c78b7e910012a5d38cd7f0a40ce25eca9ca3e0f6
NulliFieDFPV <demanufactures@web.de>
2018-05-27 19:27:47 +0200

    Shutdown

---
+++ webserver/www/includes/start.inc.php
@@ -0,0 +1,128 @@
+<?PHP
+
+function setCurrentRace($raceid) {
+    
+    $db = new PDO('mysql:host=localhost;dbname=dbpilotcheckin', 'udbpilot', 'pdbpilot');
+
+    //Pruefung
+    $sql ="SELECT * FROM traces WHERE RID=".$raceid." AND status=-1;";
+
+    $result= $db->query($sql)->fetchAll();
+    
+    if (count($result) >0) {
+        $sql ="UPDATE traces SET current=0 WHERE RID <>?; ";
+        $sql .="UPDATE traces SET current=-1 WHERE RID=?; ";
+        
+        $statement= $db->prepare($sql);
+        $statement->execute([$raceid, $raceid]);  
+    }
+    
+
+}
+
+
+function generateStats() {
+
+    $db = new PDO('mysql:host=localhost;dbname=dbpilotcheckin', 'udbpilot', 'pdbpilot');
+    
+    $tbl="<table width=\"100%\" >";
+
+    $tbl .="<tr>";
+    $tbl .="<th>Callsign</th>";
+    $tbl .="<th>Wartend</th>";
+    $tbl .="<th>Heats gesamt</th>";
+    $tbl .="<th>Heats heute</th>";
+    $tbl .="<tr>";
+        
+        
+    $sql = "SELECT * FROM traces ";
+    $sql .= "WHERE status=-1 AND current=-1 LIMIT 1";
+    //echo $sql;
+
+    $raceid=0;
+    
+    foreach ($db->query($sql) as $row) {
+        $raceid=$row["RID"];
+    }
+    
+    if ($raceid >0) {
+        $sql="SELECT * FROM tattendance a ";
+        $sql .="INNER JOIN tpilots p ";
+        $sql .="ON a.PID=p.PID ";
+        $sql .="WHERE a.RID=".$raceid." AND a.status=-1";
+    
+        foreach ($db->query($sql) as $row) {
+            $tbl .="<tr>";
+            $tbl .="<td style=\"width:30%\">".$row["callsign"]."</td>";
+            
+            if ($row["WID"]>0) {
+                $tbl .="<td style=\"text-align:center\"><div class=\"online\">&nbsp;</div></td>";
+            }
+            else {
+                $tbl .="<td style=\"text-align:center\"><div class=\"offline\">&nbsp;</div></td>";
+            }
+            
+            
+            $sql= "SELECT * FROM twaitlist WHERE AID=".$row["AID"]."; ";
+            $result= $db->query($sql)->fetchAll();
+    
+            $tbl .="<td>".count($result)."</td>";
+            
+            $sql= "SELECT * FROM twaitlist WHERE AID=".$row["AID"]." AND wait_date='".date("Y-m-d")."'; ";
+            $result= $db->query($sql)->fetchAll();
+            
+            $tbl .="<td>".count($result)."</td>";
+            $tbl .="<tr>";
+        }
+    }
+    
+    $tbl .="</table>";
+
+    return $tbl;
+
+}
+
+
+function generateCurrentRaceCombo() {
+
+    $db = new PDO('mysql:host=localhost;dbname=dbpilotcheckin', 'udbpilot', 'pdbpilot');
+    
+
+    $cbo ="<select id=\"race\" name=\"race\" onChange=\"this.form.submit()\" style=\"width:100%;\" >\n";
+    $cbo .="<option value=\"-1\">Race w&auml;hlen...</option>";
+
+    
+    $sql = "SELECT * FROM traces ";
+    $sql .= "WHERE status=-1 ";
+    $sql .= "ORDER BY race_name;";
+    //echo $sql;
+    
+    $result=$db->query($sql)->fetchAll();
+
+    foreach ($result as $row) {
+    
+        $cbo .="<option ";
+        if ($row["current"]==-1) {
+            $cbo .="selected ";
+        }
+        $cbo .="value=\"".$row["RID"]."\">".$row['race_name'];
+        
+        $sql="SELECT * FROM tattendance WHERE RID=".$row["RID"]." AND status=-1";
+        $resAnzahl=$db->query($sql)->fetchAll();
+        
+        $cbo .=" (".count($resAnzahl)." Teilnehmer ";      
+        
+        $sql="SELECT * FROM twaitlist WHERE RID=".$row["RID"]." AND status IN (-1, 1)";
+        $resAnzahl=$db->query($sql)->fetchAll();
+
+        $cbo .= count($resAnzahl)." davon eingecheckt)"; 
+        
+        $cbo .="</option>\n";
+    }
+    
+    $cbo .="</select>\n";
+    
+    return $cbo;
+}
+
+?>
--- webserver/www/index.php
+++ webserver/www/index.php
@@ -3,6 +3,27 @@
 error_reporting(E_ALL);
 
 include "includes/functions.inc.php";
+include "includes/start.inc.php";
+
+$raceid=-1;
+
+if (isset($_POST["race"])) {
+    $raceid=$_POST["race"];
+}
+else {
+    $raceid=-1;
+}
+
+
+if ($raceid>0) {
+    setCurrentRace($raceid);
+}
+
+
+$raceCombo=generateCurrentRaceCombo();
+
+$tblstats=generateStats();
+
 
 ?>
 
@@ -10,15 +31,25 @@ include "includes/functions.inc.php";
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="css/default.css">
-    <title>Pilot Checkin - MONITOR</title>
+    <title>Pilot Checkin - START</title>
 </head>
 
 <body>
     <?PHP include "navigation.php" ?>
 
 <br>
-<section class=\"main\">
 
+<section class="race">
+<form action="index.php" method="post">
+
+    <?PHP echo $raceCombo; ?>
+    
+</form>
+</section>
+<section class="main">
+<section class="stats">
+<?PHP echo $tblstats; ?>
+</section>
 </section>
 </body>
 </html>
